fcece2c0df7c9a4e61c59d0a1307d89920e332b4
Anupam Himalaya Upadhyay <anupam.upadhyay05@gmail.com>
2026-01-31 12:19:11 +0530

    update backend
    update files

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 import homeRouter from './routes/home.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
